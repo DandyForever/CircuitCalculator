558f315969611ec1866641f4d99c24a87182bea6
DandyForever <dandy4ever31@gmail.com>
2021-12-12 17:58:25 +0300

    Deploying Flex

--- input_preworker.cpp
+++ input_preworker.cpp
@@ -1,8 +1,6 @@
 #include <fstream>
 #include "input_preworker.h"
 
-
-
 std::vector<std::string> input_preworker::tokenize(const std::string& line) {
     std::vector<std::string> tokens_;
     std::stringstream line_(line);
@@ -58,10 +56,121 @@ input_preworker::input_preworker(std::istream& input, const std::string& file, i
             file_path = file_name_.substr(0, position + 1);
         }
     }
-    std::string line;
-    while (std::getline(input, line)) {
-        tokens.emplace_back(tokenize(line));
-        check_max_vertex();
+//    std::string line;
+//    while (std::getline(input, line)) {
+//        tokens.emplace_back(tokenize(line));
+//        check_max_vertex();
+//    }
+    Scanner scanner(&input);
+    for (;;) {
+        switch (scanner.get_nex_token()) {
+            case Scanner::Token::END:
+                break;
+            case Scanner::Token::INCLUDE_LINE:
+                std::stringstream include_line(scanner.YYText());
+                std::vector<std::string> include_tokens(2);
+                include_line >> include_tokens[0] >> include_tokens[1];
+                tokens.push_back(include_tokens);
+                continue;
+            case Scanner::Token::DEFINE_LINE:
+                std::string define_line(scanner.YYText());
+                define_line.replace(define_line.begin(), define_line.end(), '(', ' ');
+                define_line.replace(define_line.begin(), define_line.end(), ')', ' ');
+                define_line.replace(define_line.begin(), define_line.end(), ',', ' ');
+                std::stringstream define_line_(define_line);
+                std::vector<std::string> define_tokens(2);
+                define_line_ >> define_tokens[0] >> std::ws >> define_tokens[1] >> std::ws;
+                while (true) {
+                    std::string variable;
+                    define_line_ >> variable >> std::ws;
+                    if (variable.empty())
+                        break;
+                    define_tokens.push_back(variable);
+                }
+                tokens.push_back(define_tokens);
+                continue;
+            case Scanner::Token::INTERNAL_LINE:
+                std::stringstream internal_line(scanner.YYText());
+                std::vector<std::string> internal_tokens(3);
+                internal_line >> std::ws >> internal_tokens[1] >> std::ws >> internal_tokens[2];
+                tokens.push_back(internal_tokens);
+                continue;
+            case Scanner::Token::ELEMENT_LINE:
+                std::string element_line(scanner.YYText());
+                auto comma_it = element_line.find(',');
+                element_line.replace(element_line.begin() + comma_it, element_line.end(), 'R', ' ');
+                element_line.replace(element_line.begin() + comma_it, element_line.end(), 'C', ' ');
+                element_line.replace(element_line.begin() + comma_it, element_line.end(), 'L', ' ');
+                element_line.replace(element_line.begin() + comma_it, element_line.end(), 'V', ' ');
+                element_line.replace(element_line.begin(), element_line.end(), "--", ' ');
+                element_line.replace(element_line.begin(), element_line.end(), ',', ' ');
+                element_line.replace(element_line.begin(), element_line.end(), ';', ' ');
+                std::stringstream element_line_(element_line);
+                std::vector<std::string> element_tokens(6);
+                element_line_ >> std::ws >> element_tokens[1] >> std::ws >> element_tokens[2] >> std::ws >>
+                                element_tokens[3] >> std::ws >> element_tokens[4] >> std::ws >> element_tokens[5] >>
+                                std::ws;
+                if (!element_line_.eof()) {
+                    element_tokens.resize(8);
+                    element_line_ >> element_tokens[6] >> std::ws >> element_tokens[7];
+                }
+                tokens.push_back(element_tokens);
+                continue;
+            case Scanner::Token::EDGE_LINE:
+                std::string edge_line(scanner.YYText());
+                comma_it = edge_line.find(',');
+                edge_line.replace(edge_line.begin() + comma_it, edge_line.end(), 'R', ' ');
+                edge_line.replace(edge_line.begin() + comma_it, edge_line.end(), 'C', ' ');
+                edge_line.replace(edge_line.begin() + comma_it, edge_line.end(), 'L', ' ');
+                edge_line.replace(edge_line.begin() + comma_it, edge_line.end(), 'V', ' ');
+                edge_line.replace(edge_line.begin(), edge_line.end(), "--", ' ');
+                edge_line.replace(edge_line.begin(), edge_line.end(), ',', ' ');
+                edge_line.replace(edge_line.begin(), edge_line.end(), ';', ' ');
+                std::stringstream edge_line_(edge_line);
+                std::vector<std::string> edge_tokens(5);
+                int incoming_vertex, outcoming_vertex;
+                edge_line_ >> outcoming_vertex >> std::ws >> incoming_vertex >> std::ws >> edge_tokens[2] >> std::ws >>
+                                edge_tokens[3] >> std::ws >> edge_tokens[4] >> std::ws;
+                vertex_number = std::max(vertex_number, outcoming_vertex);
+                vertex_number = std::max(vertex_number, incoming_vertex);
+                edge_tokens[0] = std::to_string(outcoming_vertex);
+                edge_tokens[1] = std::to_string(incoming_vertex);
+                if (!edge_line_.eof()) {
+                    edge_tokens.resize(7);
+                    edge_line_ >> edge_tokens[5] >> std::ws >> edge_tokens[6];
+                }
+                tokens.push_back(edge_tokens);
+                continue;
+            case Scanner::Token::CALL_LINE:
+                std::string call_line(scanner.YYText());
+                call_line.replace(call_line.begin(), call_line.end(), '(', ' ');
+                call_line.replace(call_line.begin(), call_line.end(), ')', ' ');
+                call_line.replace(call_line.begin(), call_line.end(), ',', ' ');
+                std::stringstream call_line_(call_line);
+                std::vector<std::string> call_tokens(2);
+                call_line_ >> call_tokens[0] >> std::ws >> call_tokens[1] >> std::ws;
+                while (true) {
+                    int vertex = 0;
+                    define_line_ >> vertex >> std::ws;
+                    if (!vertex)
+                        break;
+                    vertex_number = std::max(vertex_number, vertex);
+                    define_tokens.push_back(std::to_string(vertex));
+                }
+                tokens.push_back(define_tokens);
+                continue;
+            case Scanner::Token::INVALID_LINE:
+                std::cout << "Invalid line " << scanner.lineno() << ": " << scanner.YYText() << std::endl;
+                throw UnexpectedTokenException();
+            case Scanner::Token::EOL:
+                tokens.emplace_back(1);
+                continue;
+        }
+        for (auto& token_line : tokens) {
+            for (auto& token : token_line)
+                std::cout << '\'' << token << '\'';
+            std::cout << std::endl;
+        }
     }
 }
 
@@ -109,15 +218,15 @@ void input_preworker::perform_prework() {
 }
 
 void input_preworker::add_edge(std::vector<std::string> &token_line) {
-    check_edge_token_line(token_line);
+//    check_edge_token_line(token_line);
     for (auto& token : token_line)
-        output << token;
+        output << token << ' ';
     output << '\n';
 }
 
 void input_preworker::substitute_element(const std::vector<std::string> &token_line) {
     std::string element_name = token_line[0];
-    check_element_token_line(token_line);
+//    check_element_token_line(token_line);
     check_element_existance(element_name);
     auto &element_ = elements[element_name];
     std::map<std::string, std::string> parameter_value_mapping = map_parameters_values(token_line, element_);
@@ -137,10 +246,12 @@ void input_preworker::print_message_prefix() const { std::cout << file_name_ << 
 void input_preworker::update_output_with_element_edges(input_preworker::element &element_,
                                                        const std::map<std::string, std::string> &parameter_value_mapping) {
     for (auto &element_edges: element_.edges) {
-        output << parameter_value_mapping.at(element_edges[1]) << element_edges[2] <<
-               parameter_value_mapping.at(element_edges[3]);
-        for (size_t edge_attribute = 4; edge_attribute < element_edges.size(); edge_attribute++)
-            output << element_edges[edge_attribute];
+        output << parameter_value_mapping.at(element_edges[0]) << " " <<
+               parameter_value_mapping.at(element_edges[2]);
+//        for (size_t edge_attribute = 4; edge_attribute < element_edges.size(); edge_attribute++)
+//            output << element_edges[edge_attribute];
+        for (size_t edge_attribute = 3; edge_attribute < element_edges.size(); edge_attribute++)
+            output << ' ' << element_edges[edge_attribute];
         output << '\n';
     }
 }
@@ -148,7 +259,10 @@ void input_preworker::update_output_with_element_edges(input_preworker::element 
 std::map<std::string, std::string>
 input_preworker::map_parameters_values(const std::vector<std::string> &token_line, input_preworker::element &element_) {
     std::vector<std::string> element_values;
-    for (size_t value_index = 2; value_index < token_line.size() - 1; value_index += 2) {
+//    for (size_t value_index = 2; value_index < token_line.size() - 1; value_index += 2) {
+//        element_values.push_back(token_line[value_index]);
+//    }
+    for (size_t value_index = 2; value_index < token_line.size(); value_index++) {
         element_values.push_back(token_line[value_index]);
     }
     check_parameters_number(element_, element_values);
@@ -175,7 +289,7 @@ void input_preworker::check_parameters_number(const input_preworker::element &el
 }
 
 void input_preworker::manage_include(const std::vector<std::string> &token_line) {
-    check_include_token_line(token_line);
+//    check_include_token_line(token_line);
     std::string file_name = file_path + token_line[1];
     std::ifstream file_;
     file_.open(file_name);
@@ -202,10 +316,14 @@ void input_preworker::manage_include(const std::vector<std::string> &token_line)
 
 void input_preworker::create_element() {
     auto &token_line = tokens[line_index];
-    check_define_token_line(token_line);
+//    check_define_token_line(token_line);
     element element_;
     element_.name = token_line[1];
-    for (size_t parameter_index = 3; parameter_index < token_line.size() - 1; parameter_index += 2) {
+//    for (size_t parameter_index = 3; parameter_index < token_line.size() - 1; parameter_index += 2) {
+//        element_.parameters.push_back(token_line[parameter_index]);
+//        element_.variables.insert(token_line[parameter_index]);
+//    }
+    for (size_t parameter_index = 2; parameter_index < token_line.size(); parameter_index++) {
         element_.parameters.push_back(token_line[parameter_index]);
         element_.variables.insert(token_line[parameter_index]);
     }
@@ -226,11 +344,11 @@ void input_preworker::create_element() {
             continue;
         }
         if (token_line[1] == "internal") {
-            check_internal_token_line(token_line);
+//            check_internal_token_line(token_line);
             element_.internal_variables.push_back(token_line[2]);
             element_.variables.insert(token_line[2]);
         } else {
-            check_define_edge_token_line(token_line);
+//            check_define_edge_token_line(token_line);
             check_parameter_defined(token_line, element_);
             element_.edges.push_back(token_line);
         }
@@ -251,9 +369,9 @@ void input_preworker::check_parameter_defined(const std::vector<std::string> &to
         std::cout << "Parameter " << token_line[1] << " in element " << element_.name << "is undefined" << std::endl;
         throw UndefinedVariableException();
     }
-    if (!element_.variables.count(token_line[3])) {
+    if (!element_.variables.count(token_line[2])) {
         print_message_prefix();
-        std::cout << "Parameter " << token_line[3] << " in element " << element_.name << "is undefined" << std::endl;
+        std::cout << "Parameter " << token_line[2] << " in element " << element_.name << "is undefined" << std::endl;
         throw UndefinedVariableException();
     }
 }
--- input_preworker.h
+++ input_preworker.h
@@ -7,6 +7,7 @@
 #include <map>
 #include <vector>
 #include <set>
+#include "scanner.h"
 
 class input_preworker {
 public:
